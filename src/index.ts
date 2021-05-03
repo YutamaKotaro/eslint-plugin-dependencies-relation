3463a1765804f3a381fbe8ce22b2899ba34dab41
YutamaKotaro <california.dreamin.never.die@gmail.com>
2021-05-03 18:24:45 +0900

    add recommended

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,22 @@ import {importLimitation} from "./rules/import";
 export = {
   rules: {
     import: importLimitation,
+  },
+  configs: {
+    recommended: {
+      rules: {
+        'dependency-relation/import': 2
+      },
+      parser: '@typescript-eslint/parser',
+      parserOptions: { sourceType: 'module' },
+      plugins: ['@typescript-eslint'],
+      settings: {
+        "import/resolver": {
+          node: {
+            "extensions": [".ts", ".tsx", ".js", ".jsx"]
+          }
+        }
+      }
+    }
   }
 }
--- src/rules/utils/fileChecker.ts
+++ src/rules/utils/fileChecker.ts
@@ -1,4 +1,3 @@
-
 const DEFAULT_IGNORE_PATTERN = '(.spec|.test)(.ts|.tsx|.js)$'
 export function ignoreFile(filePath: string): boolean {
   const reg = new RegExp(DEFAULT_IGNORE_PATTERN)
