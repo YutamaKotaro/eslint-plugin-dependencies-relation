406b995c5a2c9a2893d1ab56840e82fd068f266f
YutamaKotaro <california.dreamin.never.die@gmail.com>
2021-05-15 15:10:26 +0900

    add require allowRootTS case

--- tests/rules/require/arrow_root.spec.ts
+++ tests/rules/require/arrow_root.spec.ts
@@ -6,7 +6,7 @@ import { createErrorMessage } from '../../../src/utils/compare'
 const ruleTester = new RuleTester()
 
 // @ts-ignore
-ruleTester.run('import/js-allow-root-path case', requireLimitation, {
+ruleTester.run('require/js-allow-root-path case', requireLimitation, {
   valid: [
     {
       ...createJsOption('./allowRootJs/require/app.js'),
@@ -45,6 +45,54 @@ ruleTester.run('import/js-allow-root-path case', requireLimitation, {
         {
           message: createErrorMessage(
             './allowRootJs/require/forbid/utils',
+            'require'
+          ),
+        },
+      ],
+    },
+  ],
+})
+
+// @ts-ignore
+ruleTester.run('require/ts-allow-root-path case', requireLimitation, {
+  valid: [
+    {
+      ...createJsOption('./allowRootTs/require/app.ts'),
+      code: `
+          const {test} = require('./forbid/utils')
+      `,
+    },
+    {
+      ...createJsOption('./allowRootTs/require/app.ts'),
+      code: `
+        const {test} = require('./forbid')
+      `,
+    },
+  ],
+  invalid: [
+    {
+      ...createJsOption(),
+      code: `
+        const {test} = require('./allowRootTs/require/forbid')
+      `,
+      errors: [
+        {
+          message: createErrorMessage(
+            './allowRootTs/require/forbid',
+            'require'
+          ),
+        },
+      ],
+    },
+    {
+      ...createJsOption(),
+      code: `
+        const {test} = require('./allowRootTs/require/forbid/utils')
+      `,
+      errors: [
+        {
+          message: createErrorMessage(
+            './allowRootTs/require/forbid/utils',
             'require'
           ),
         },
